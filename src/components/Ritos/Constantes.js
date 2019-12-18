a9cd684b10a3d4fa429ae23e241c03f0cb75553e
Bárbara <martins.barbara04@gmail.com>
2019-12-18 17:34:45 -0300

    Remove old files

--- src/components/Ritos/Constantes.js
+++
@@ -1,7 +0,0 @@
-const Squads = Object.freeze([
-    { name: 'Origem - Ride Sharing - Easy Mode' },
-    { name: 'Origem - Ride Sharing - Vida Boa' },
-    { name: 'Origem - Ride Sharing - Driver Rewards' },
-]);
-
-export default Squads;
